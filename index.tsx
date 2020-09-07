c1d7b4705a11214fdf732512d3ecf89e42e9e5e3
Binatik <47430210+Binatik@users.noreply.github.com>
2020-09-07 14:31:02 +0300

    res

--- index.tsx
+++ index.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 
-const data = {
-  content: "данные или текст",
-  children: [
-    {
-      content: "данные или текст для вложенного элемента 1",
-      children: [],
-    },
-    {
-      content: "данные или текст для вложенного элемента 2",
-      children: [],
-    },
-  ],
-};
-
 function TreeNode(props) {
-  const content = props.data.content; //Заношу data.content в переменную myData
-  const children = props.data.children; //Заношу data.ребенок в переменную myData
+  const content = props.data.content; 
+  const children = props.data.children; 
   let item;
   let treeNodesList;
 
@@ -35,7 +21,6 @@ function TreeNode(props) {
   );
 }
     
-//Возвращаем из DeepTree компонент TreeNode с ключом data : {obj}
 export function DeepTree(props) {
   return <TreeNode data={props.data} />;
 }
