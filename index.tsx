898a5d30a7048973702d9a75dfd646d932c2c5c2
Konstantin Dyachenko <drakonard@gmail.com>
2020-09-10 20:01:01 +0300

    Refactoring.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 function TreeNode(props) {
-  const content = props.data.content; 
-  const children = props.data.children; 
+  const content = props.data.content;
+  const children = props.data.children;
   let item;
   let treeNodesList;
-
   if (content !== undefined) {
     item = <div>{content}</div>;
   }
   if (children && children.length > 0) {
-    const treeNodes = children.map((element) => <TreeNode data={element} />);
+    const treeNodes = children.map(element => <TreeNode data={element} />);
     treeNodesList = <ul>{treeNodes}</ul>;
   }
   return (
@@ -20,9 +19,9 @@ function TreeNode(props) {
     </li>
   );
 }
-    
+
 export function DeepTree(props) {
   return <TreeNode data={props.data} />;
 }
-  
-export default DeepTree; 
+
+export default DeepTree;
