600fd03b20f75607f4f3325a878726949ce3f2e4
Binatik <47430210+Binatik@users.noreply.github.com>
2020-10-31 18:38:25 +0300

    Первая работа с  next.js
    Пришлось переписать структуру проекта и отклонится от вебпака.  Использовал npx create-next-app

---
+++ example/components/tree/DeepTree.js
@@ -0,0 +1,17 @@
+import React from "react";
+/*
+import TreeNode from "./TreeNode.JS"
+
+export default function DeepTree({data}) {
+    return (
+        data.map((object, index) =>
+            (
+                <ul key={index}>
+                    <TreeNode data={object}/>
+                </ul>
+            )
+        )
+    );
+}
+
+ */
---
+++ example/pages/index.js
@@ -0,0 +1,43 @@
+import DeepTree from "../components/tree/DeepTree";
+import React from "react";
+
+/*
+interface DataNode {
+    readonly content: any
+    readonly children: DataNode[]
+}
+
+const data: DataNode[] = [
+    {
+        content: "Text of a first level of the first element",
+        children: [
+            {
+                content: "Text of a second level of the first element",
+                children: [],
+            },
+            {
+                content: "Text of a second level of the first element",
+                children: [],
+            },
+        ],
+    },
+    {
+        content: "Text of a first level of the first element",
+        children: [
+            {
+                content: "Text of a second level of the first element",
+                children: [],
+            },
+            {
+                content: "Text of a second level of the first element",
+                children: [],
+            },
+        ],
+    },
+]
+*/
+
+export default function Index() {
+    //return <DeepTree data={'data'}/>
+    return <h1>Hello Next Test</h1>
+}
