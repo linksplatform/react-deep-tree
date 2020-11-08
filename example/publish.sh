d2cea290e8a1f87bf00a7c44fae366f1d601ebc2
Binatik <47430210+Binatik@users.noreply.github.com>
2020-11-08 16:14:17 +0300

    added sh script for cloning

---
+++ example/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+TARGET_BRANCH="gh-pages"
+COMMIT_USER_NAME="linksplatform"
+REPOSITORY="github.com/linksplatform/$REPOSITORY_NAME"
+SHA=$(git rev-parse --verify HEAD)
+
+git clone "https://$REPOSITORY" out
+cd out || exit
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+npm i
+npm run export
+cd out
+
+
+# Now let's go have some fun with the cloned repo
+git config user.name "$COMMIT_USER_NAME"
+git remote rm origin
+git remote add origin "https://linksplatform:$GITHUB_TOKEN@$REPOSITORY.git"
+
+git add --all
+git commit -m "Deploy to GitHub Pages: $SHA"
+
+git push "https://linksplatform:$GITHUB_TOKEN@$REPOSITORY.git" "$TARGET_BRANCH"
+cd ..
