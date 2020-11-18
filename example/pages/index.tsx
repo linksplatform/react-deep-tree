e9a7e2751f31ccc201bb690ec2b2fbdfa57bf489
Binatik <47430210+Binatik@users.noreply.github.com>
2020-11-18 13:21:20 +0300

    Refactoring

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,36 +1,36 @@
+import React from "react";
 import DeepTree from "react-deep-tree";
 import type { DataNode } from "react-deep-tree";
-import React from "react";
 
-const data: DataNode[] = [
-    {
-        content: "Text of a first level of the first element",
-        children: [
-            {
-                content: "Text of a second level of the first element",
-                children: [],
-            },
-            {
-                content: "Text of a second level of the first element",
-                children: [],
-            },
-        ],
-    },
-    {
-        content: "Text of a first level of the second element",
-        children: [
-            {
-                content: "Text of a second level of the second element",
-                children: [],
-            },
-            {
-                content: "Text of a second level of the second element",
-                children: [],
-            },
-        ],
-    },
-]
+const data : DataNode[] = [
+  {
+    content: 'Text of a first level of the first element',
+    children: [
+      {
+        content: 'Text of a second level of the first element',
+        children: [],
+      },
+      {
+        content: 'Text of a second level of the first element',
+        children: [],
+      },
+    ],
+  },
+  {
+    content: 'Text of a first level of the second element',
+    children: [
+      {
+        content: 'Text of a second level of the second element',
+        children: [],
+      },
+      {
+        content: 'Text of a second level of the second element',
+        children: [],
+      },
+    ],
+  },
+];
 
 export default function Index() {
-    return <DeepTree data={data}/>
+  return <DeepTree data={data} />;
 }
--- index.tsx
+++ index.tsx
@@ -1,37 +1,36 @@
-// @ts-ignore
 import React from 'react';
 
 interface DataNode {
-    readonly content: any
-    readonly children: DataNode[]
+  readonly content: any;
+  readonly children: DataNode[];
 }
 export type { DataNode };
 
-export function TreeNode({data}) {
-    const {content, children} = data;
-    let item;
-    let treeNodesList;
+export function TreeNode({ data }) {
+  const { content, children } = data;
+  let item;
+  let treeNodesList;
 
-    if (content !== undefined) {
-        item = <div>{content}</div>;
-    }
-    if (children && children.length > 0) {
-        // @ts-ignore
-        const treeNodes = children.map((element, index) => <TreeNode data={element} key={index} />);
-        treeNodesList = <ul>{treeNodes}</ul>;
-    }
-    return (
-        <li>
-            {item}
-            {treeNodesList}
-        </li>
-    );
+  if (content !== undefined) {
+    item = <div>{content}</div>;
+  }
+  if (children && children.length > 0) {
+
+    const treeNodes = children.map((element, index) => <TreeNode data={element} key={index} />);
+    treeNodesList = <ul>{treeNodes}</ul>;
+  }
+  return (
+    <li>
+      {item}
+      {treeNodesList}
+    </li>
+  );
 }
 
-export function DeepTree({data}) {
-    return (
-        <ul> {data.map((object, index) => <TreeNode key={index} data={object} />)} </ul>
-    );
+export function DeepTree({ data }) {
+  return (
+    <ul> {data.map((object, index) => <TreeNode key={index} data={object} />)} </ul>
+  );
 }
 
 export default DeepTree;
