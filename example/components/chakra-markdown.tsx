728d5198ca372af444620eb06ccade9c024eb3ae
Konstantin Diachenko <drakonard@gmail.com>
2024-10-18 17:44:38 +0530

    First working prototype of fit to content height adjustment

--- example/components/chakra-markdown.tsx
+++ example/components/chakra-markdown.tsx
@@ -1,14 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Box, Text, Link as ChakraLink, Heading, useColorModeValue, useColorMode } from '@chakra-ui/react';
 import MonacoEditor from '@monaco-editor/react';
 import ReactMarkdown from 'react-markdown';
 import debounce from 'lodash/debounce';
 
+function calculateChildrenHeight(parentDiv) {
+  let childrenHeight = 0;
+  const children = parentDiv.children;
+  for (let i = 0; i < children.length; i++) {
+    childrenHeight += children[i].offsetHeight;
+  }
+  return childrenHeight;
+}
+
 const ChakraMarkdown = React.memo<any>(({ content }) => {
-  const { colorMode } = useColorMode();
-  const editorRef = useRef();
-  const monacoTheme = colorMode === 'dark' ? 'vs-dark' : 'vs-light';
-  const [editorHeight, setEditorHeight] = useState(0);
   const renderers = {
     h1: ({ children }) => <Heading as="h1" size="xl" mb={2}>{children}</Heading>,
     h2: ({ children }) => <Heading as="h2" size="lg" mb={2}>{children}</Heading>,
@@ -19,32 +24,88 @@ const ChakraMarkdown = React.memo<any>(({ content }) => {
       </ChakraLink>
     ),
     code: ({ inline, children, className }) => {
+      const { colorMode } = useColorMode();
+      const monacoTheme = colorMode === 'dark' ? 'vs-dark' : 'vs-light';
+
+      const codeContainerRef = useRef(null);
+
+
+
       const language = className ? className.replace('language-', '') : 'plaintext';
       const code = String(children).trim();
+
+      const lines = code.split('\n').length;
+      const lineHeight = 18; // Approximate height of each line in pixels
+      // const horizonalScrollBarHeight = 12;
+      // const totalHeight = lines * lineHeight + horizonalScrollBarHeight;
+      const defaultHeight = lines * lineHeight;
+      // setEditorHeight(`${totalHeight}px`);
+
+      const heightRef = useRef(defaultHeight);
+      const [editorHeight, setEditorHeight] = useState(defaultHeight);
+      heightRef.current = editorHeight;
+
       if (inline) {
         return <Text as="code" bg={useColorModeValue("gray.100", "gray.700")} p="1" borderRadius="md">{code}</Text>;
       }
+
+      const updateHeight = useCallback(() => {
+        // setEditorHeight(defaultHeight);
+        // setTimeout(() => {
+        const childrenHeight = calculateChildrenHeight(codeContainerRef.current);
+        const scrollHeight = codeContainerRef.current?.scrollHeight;
+        const offsetHeight = codeContainerRef.current?.offsetHeight;
+        // const newHeight = Math.max(childrenHeight);
+        console.log('resize', 'childrenHeight', childrenHeight);
+        console.log('resize', 'scrollHeight', scrollHeight);
+        console.log('resize', 'clientHeight', codeContainerRef.current.clientHeight);
+        console.log('resize', 'offsetHeight', codeContainerRef.current.offsetHeight);
+        console.log('resize', 'editorHeight', heightRef.current);
+
+        // if (editorHeight < childrenHeight) {
+        //   setEditorHeight(childrenHeight);
+        // } else {
+        //   const scrollHeight = codeContainerRef.current?.scrollHeight;
+        //   const offsetHeight = codeContainerRef.current?.offsetHeight;
+        //   const newHeight = Math.max(childrenHeight, scrollHeight, offsetHeight);
+        //   setEditorHeight(newHeight);
+        // }
+        // if (newHeight > defaultHeight && editorHeight !== newHeight) {
+
+        // }
+
+        if (heightRef.current <= childrenHeight) {
+          const newHeight = Math.max(childrenHeight, scrollHeight);
+          setEditorHeight(newHeight);
+        } else {
+          const newHeight = Math.min(childrenHeight, scrollHeight);
+          setEditorHeight(newHeight);
+        }
+
+
+        // const scrollHeight = codeContainerRef.current?.scrollHeight;
+        // console.log('resize', 'scrollHeight', scrollHeight);
+        // setEditorHeight(scrollHeight);
+
+        // }, 0);
+      }, [editorHeight]);
+
       useEffect(() => {
-        const lines = code.split('\n').length;
-        const lineHeight = 18; // Approximate height of each line in pixels
-        // const horizonalScrollBarHeight = 12;
-        // const totalHeight = lines * lineHeight + horizonalScrollBarHeight;
-        const totalHeight = lines * lineHeight;
-        // setEditorHeight(`${totalHeight}px`);
-      }, [children]);
+        return () => {
+          window.removeEventListener('resize', updateHeight);
+          console.log('unmount');
+          // updateHeight.cancel(); // Cancels the debounced function
+        };
+      }, []);
       return (
         <Box
           border="1px solid"
           borderColor={useColorModeValue("gray.200", "gray.600")}
           borderRadius="0"
           overflow="visible"
-          onResize={() => {
-            console.log('resize');
-            setEditorHeight(0);
-          }}
-          my={2}>
+          my={2}
+        >
           <MonacoEditor
-
             height={editorHeight}
             language={language}
             theme={monacoTheme}
@@ -55,16 +116,15 @@ const ChakraMarkdown = React.memo<any>(({ content }) => {
               scrollBeyondLastLine: false,
               lineNumbers: "off",
               scrollbar: { vertical: 'hidden' },
-              overviewRulerLanes: 0,             // Disable the overview ruler
-              hideCursorInOverviewRuler: true,   // Hide cursor in the ruler
-              folding: false,                    // Disable code folding
-              glyphMargin: false,                // Hide the glyph margin (left margin with icons)
-              renderLineHighlight: 'none',       // Disable the current line highlight
+              overviewRulerLanes: 0,
+              hideCursorInOverviewRuler: true,
+              folding: false,
+              glyphMargin: false,
+              renderLineHighlight: 'none',
               cursorWidth: 1,
               wordWrap: 'on',
             }}
-            onMount={(editor, monaco) => {
-              console.log('mount');
+            onMount={(editor) => {
               const keepIds = ["editor.action.clipboardCopyAction"];
               const contextmenu = editor.getContribution('editor.contrib.contextmenu');
               const realMethod = contextmenu._getMenuActions;
@@ -75,58 +135,27 @@ const ChakraMarkdown = React.memo<any>(({ content }) => {
                 });
               };
 
-              console.log('editor', editor);
-              const el = editor._domElement;
-              console.log('el', el);
-              const codeContainer = el?.querySelector?.('.view-lines');
-              console.log('codeContainer', codeContainer);
+              const codeContainer = editor._domElement?.querySelector('.view-lines');
+              codeContainerRef.current = codeContainer; // Assign the ref
+              // if (codeContainer?.offsetHeight > 0) {
+              //   setEditorHeight(codeContainer.offsetHeight);
+              // }
 
+              console.log(editor);
 
-              let prevLineCount = 0;
+              window.addEventListener('resize', updateHeight);
 
-              const LINE_HEIGHT = 18;
-              const CONTAINER_GUTTER = 10;
-
-              window.addEventListener('resize', debounce(function (event) {
-                // Your code here
-                // console.log('Window resized to: ', window.innerWidth, window.innerHeight);
-                // const lines = code.split('\n').length;
-                // const lineHeight = 18; // Approximate height of each line in pixels
-                // // const horizonalScrollBarHeight = 12;
-                // // const totalHeight = lines * lineHeight + horizonalScrollBarHeight;
-                // const totalHeight = lines * lineHeight;
-                // if (editorHeight === totalHeight) {
-                //   setEditorHeight(totalHeight-lineHeight); // Force change
-                // } else {
-                //   setEditorHeight(totalHeight);
-                // }
-
-                if (codeContainer.offsetHeight > 0) {
-                  // el.style.height = codeContainer.offsetHeight + 'px';
-                  const newHeight = codeContainer.offsetHeight;
-                  if (editorHeight !== newHeight) {
-                    setEditorHeight(newHeight);
-                  }
-                  // editor.layout();
-                }
-              }, 200));
-
-              editor.onDidChangeModelDecorations(() => {
-                // wait until dom rendered
-                setTimeout(() => {
-                  console.log('codeContainer.offsetHeight', codeContainer?.offsetHeight);
-                  if (codeContainer.offsetHeight > 0) {
-                    // el.style.height = codeContainer.offsetHeight + 'px';
-                    const newHeight = codeContainer.offsetHeight;
-                    setEditorHeight(newHeight);
-                    // editor.layout();
-                  }
-                  // const height =
-                  //   codeContainer.childElementCount > prevLineCount
-                  //     ? codeContainer.offsetHeight // unfold
-                  //     : codeContainer.childElementCount * LINE_HEIGHT + CONTAINER_GUTTER; // fold
-                  // prevLineCount = codeContainer.childElementCount;
-                }, 10);
+              const eventHandler = editor.onDidChangeModelDecorations(() => {
+                const childrenHeight = calculateChildrenHeight(codeContainerRef.current);
+                const scrollHeight = codeContainerRef.current?.scrollHeight;
+                const offsetHeight = codeContainerRef.current?.offsetHeight;
+                const newHeight = Math.max(childrenHeight, scrollHeight, offsetHeight);
+                console.log('onmount', 'childrenHeight', childrenHeight);
+                console.log('onmount', 'scrollHeight', scrollHeight);
+                console.log('onmount', 'clientHeight', codeContainerRef.current.clientHeight);
+                console.log('onmount', 'offsetHeight', codeContainerRef.current.offsetHeight);
+                setEditorHeight(newHeight);
+                eventHandler.dispose();
               });
             }}
           />
@@ -134,6 +163,7 @@ const ChakraMarkdown = React.memo<any>(({ content }) => {
       );
     },
   };
+
   return <ReactMarkdown components={renderers} children={content} />;
 });
 
